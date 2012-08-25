bf3acbaf2f7026f4376d2d1494603e77202c92ae
Pedro Romano <pedro.romano@digitaluna.net>
2012-08-26 00:20:32 +0100

    Further enhancements to bootstrap and install scripts. This should now enable going as far as having 'chef' installed without any user intervention apart from entering passwords for authentication.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get -y install python-software-properties
+apt-get -y install debconf-utils python-software-properties
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 83EF826A
 add-apt-repository "deb http://apt.opscode.com/ precise-0.10 main"
 apt-get update
+echo "chef chef/chef_server_url string none" | debconf-set-selections
 apt-get -y install opscode-keyring chef
