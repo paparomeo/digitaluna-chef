bcde9f8eb2ea0143d34d1955966e25c5db7bcec0
Pedro Romano <pedro.romano@digitaluna.net>
2012-08-26 22:54:18 +0100

    New recipe to reconfigure installed OpenSSH daemon to disallow root logins and tunnelled clear text passwords. Add the recipe to the run list.

---
+++ cookbooks/openssh/recipes/reconfigure-sshd.rb
@@ -0,0 +1,17 @@
+# reconfigure sshd with desired defaults
+ruby_block 'reconfigure_sshd' do
+    block do
+        f = Chef::Util::FileEdit.new("/etc/ssh/sshd_config")
+        # don't allow login as root
+        f.search_file_replace(/^PermitRootLogin yes/, 'PermitRootLogin no')
+        # disable tunneled clear text passwords
+        f.search_file_replace(/^#PasswordAuthentication yes/, 'PasswordAuthentication no')
+        f.write_file
+    end
+    action :create
+end
+
+# restart sshd to reload configuration
+service 'ssh' do
+    action :restart
+end
