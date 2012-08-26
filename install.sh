0816ba99768b0cc6813732efb10ef075023057f2
Pedro Romano <pedro.romano@digitaluna.net>
2012-08-26 14:02:15 +0100

    Create symbolic link to the repository's 'solo.rb' (empty for now) 'chef-solo' configuration file at its default location '/etc/chef'.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+CHEF_ETC_DIR=/etc/chef
+CHEF_LIB_DIR=/var/lib/chef
+CHEF_SOLO_CFG=${CHEF_LIB_DIR}/solo.rb
+
 apt-get -y install debconf-utils python-software-properties
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 83EF826A
 add-apt-repository "deb http://apt.opscode.com/ precise-0.10 main"
 apt-get update
 echo "chef chef/chef_server_url string none" | debconf-set-selections
 apt-get -y install opscode-keyring chef
+ln -s ${CHEF_SOLO_CFG} ${CHEF_ETC_DIR}
