1b84cfb5ec37b8ef9ef7baf4b386ba39b17b6a04
Pedro Romano <pedro.romano@digitaluna.net>
2012-08-27 00:10:28 +0100

    Use exact output of 'debconf-get-selections' to prepare installation responses without replacing tabs with spaces.

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ apt-get -y install debconf-utils python-software-properties
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 83EF826A
 add-apt-repository "deb http://apt.opscode.com/ precise-0.10 main"
 apt-get update
-echo "chef chef/chef_server_url string none" | debconf-set-selections
+echo "chef	chef/chef_server_url	string	none" | debconf-set-selections
 apt-get -y install opscode-keyring chef
 ln -s ${CHEF_SOLO_CFG} ${CHEF_ETC_DIR}
