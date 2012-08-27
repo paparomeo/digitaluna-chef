5e8078c77e2cb1a7375da1bfd0de1c6fa201882d
Pedro Romano <pedro.romano@digitaluna.net>
2012-08-27 12:10:10 +0100

    Set 'chef-solo' cache directory to live under '/var/cache' to prevent polluting '/var/lib/chef'.

--- solo.rb
+++ solo.rb
@@ -1,9 +1,10 @@
 file = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
 base_dir = File.expand_path(File.dirname(file))
 
-data_bag_path File.join(base_dir, 'data-bags')
-file_cache_path base_dir
 # the cookbook path is searched in reverse order, i.e. later entries take
 # precedence over earlier ones
 cookbook_path [File.join(base_dir, 'cookbooks'),
                File.join(base_dir, 'site-cookbooks')]
+data_bag_path File.join(base_dir, 'data-bags')
+# put chef cache under /var/cache
+file_cache_path File.join('/var', 'cache', File.basename(base_dir))
